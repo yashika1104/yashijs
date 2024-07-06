c54135e663a459130e463e2e94e7b176efc07688
yashika1104 <sainiyashika93@gmail.com>
2024-07-06 10:46:51 +0000

    kkkkghggg

---
+++ 01_basic/comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1)-t
+// console.log(2<1)-F
+// console.log(2>"1")-T
+// console.log(2==2)-T
+// console.log(9!=8)-T
+// console.log(9<=8)-F
+// console.log(8>=7)-T
+// console.log(null>0)-F
+// console.log(null==0)-F
+// console.log(null>=0)-T
+// console.log(undefined>0)-F
+// console.log(undefined==0)-F
+// console.log(undefined>=0)-F
+//=== CHECK KREGA DATATYPE
+console.log("5">false);
---
+++ 01_basic/operation.js
@@ -0,0 +1,4 @@
+// let a=3
+// let b=-a
+// console.log(b)
+console.log(+false);
